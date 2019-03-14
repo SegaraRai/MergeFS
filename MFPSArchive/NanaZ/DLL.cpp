9ed181e741865805a4ad5423e8dd2af9dd0a8e03
SegaraRai <SegaraRai@users.noreply.github.com>
2019-03-14 18:41:21 +0900

    [MFPSArchive] 不要なusing namespaceを削除

--- MFPSArchive/NanaZ/DLL.cpp
+++ MFPSArchive/NanaZ/DLL.cpp
@@ -24,8 +24,6 @@ DLL::~DLL() {
 
 
 FARPROC DLL::GetProc(LPCSTR ProcName) const {
-  using namespace std::literals;
-
   const auto address = GetProcAddress(hModule, ProcName);
   if (!address) {
     throw std::runtime_error(u8"failed to get procedure address of "s + ProcName);
